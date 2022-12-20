d9db29fc88a99eff93ff040ad080302d614b4e9a
Alexandr Nebajkin <Alexander.Nebajkin@esterdev.com>
2022-12-20 22:52:33 +0300

    Add PositiveSum.

---
+++ PositiveSum.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace CodeWars
+{
+	public class PositiveSumSol
+	{
+		public static int PositiveSum(int[] arr)
+		{
+			return arr.Where(x => x > 0).Sum();
+		}
+	}
+
+	[TestFixture]
+	public class PositiveSumTests
+	{
+		[Test]
+		[TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15)]
+		[TestCase(new int[] { 1, -2, 3, 4, 5 }, ExpectedResult = 13)]
+		[TestCase(new int[] { -1, 2, 3, 4, -5 }, ExpectedResult = 9)]
+		[TestCase(new int[] { }, ExpectedResult = 0)]
+		[TestCase(new int[] { -1, -2, -3, -4, -5 }, ExpectedResult = 0)]
+		public static int ExampleTest(int[] arr)
+		{
+			return PositiveSumSol.PositiveSum(arr);
+		}
+	}
+}
