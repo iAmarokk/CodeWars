4f66ce75360acb94702e655608ab6d36d5ce2054
Alexandr Nebajkin <Alexander.Nebajkin@esterdev.com>
2023-01-12 18:14:17 +0300

    Add NewestFolder.

---
+++ Code/4kyu/NewestFolder.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace CodeWars._4kyu
+{
+	internal class NewestFolder
+	{
+		public static string Folder(string path)
+		{
+			var item = new DirectoryInfo(path).GetDirectories()
+							.OrderByDescending(d => d.LastWriteTime).First();
+			return item.FullName;
+		}
+	}
+
+	public class NewestFolderTest
+	{
+		[Test]
+		public void SimpleTest()
+		{
+			Assert.AreEqual("E:\\Temp\\FinistSBP_221122_1.3.1.3", NewestFolder.Folder("E:\\Temp"));
+		}
+	}
+}
